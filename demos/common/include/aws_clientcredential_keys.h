912a4200dd0141bd1a0234238215c84955e6d7d2
Mark Cridge <mcridge@eseye.com>
2018-11-15 11:29:15 +0000

    added with errors fixed

---
+++ demos/common/include/aws_clientcredential_keys.h
@@ -0,0 +1,17 @@
+/*
+ * PEM-encoded client certificate.
+ *
+ * Must include the PEM header and footer:
+ * "-----BEGIN CERTIFICATE-----"
+ * "...base64 data..."
+ * "-----END CERTIFICATE-----";
+ */
+/*
+ * PEM-encoded Just-in-Time Registration (JITR) certificate (optional).
+ *
+ * If used, must include the PEM header and footer:
+ * "-----BEGIN CERTIFICATE-----"
+ * "...base64 data..."
+ * "-----END CERTIFICATE-----";
+ */
+static const char * clientcredentialJITR_DEVICE_CERTIFICATE_AUTHORITY_PEM = NULL;
