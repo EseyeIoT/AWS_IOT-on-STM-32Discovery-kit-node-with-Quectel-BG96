a222b4ddf142ba6973d71c1490e5f492f93ed16c
Mark Cridge <mcridge@eseye.com>
2019-03-07 11:16:14 +0000

    try to connect forever

--- demos/st/stm32l475_discovery/common/application_code/eseye.c
+++ demos/st/stm32l475_discovery/common/application_code/eseye.c
@@ -231,7 +231,7 @@ int cellular_init(void)
 				vTaskDelay(pdMS_TO_TICKS(10000));
 			}
 		}
-		while (c2cConnectCounter < CELL_CONNECT_MAX_ATTEMPT_COUNT);
+		while (1);//c2cConnectCounter < CELL_CONNECT_MAX_ATTEMPT_COUNT);
 
 		//BSP_LED_Off(LED_GREEN);
 		if (ret == C2C_RET_OK)
